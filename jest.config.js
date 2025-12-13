826b65178bb13e87799b48d62c8b02936e4baa48
Igor <trololol-lolo@mail.ru>
2025-12-13 13:50:35 +1000

    lint

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,31 @@
 /** @type {import('jest').Config} */
 export default {
-  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.js'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  
+
   collectCoverageFrom: [
     'src/**/*.js',
   ],
-  
+
   // Ключевые настройки для правильных путей:
   moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1'
+    '^src/(.*)$': '<rootDir>/src/$1',
   },
-  
+
   // Устанавливаем корневой каталог
   rootDir: process.cwd(),
-  
+
   // Используем process.cwd() вместо __dirname
   modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
-  
+
   // Добавляем для правильной работы с путями
   coveragePathIgnorePatterns: [
     '/node_modules/',
-    '/coverage/'
+    '/coverage/',
   ],
-  
+
   transform: {},
 }
