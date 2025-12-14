0ecae49c8ad7d7ba4ad15e1bf1e64bad9457b36f
Igor <trololol-lolo@mail.ru>
2025-12-14 11:14:12 +1000

    fix

--- jest.config.js
+++ jest.config.js
@@ -4,26 +4,26 @@ export default {
   testMatch: ['**/__tests__/**/*.test.js'],
   collectCoverage: true,
   coverageDirectory: './coverage',
-  coverageReporters: ['json', ['lcov', { projectRoot: process.cwd() }], 'text'],
+  coverageReporters: ['json', ['lcov', { projectRoot: process.cwd() }], 'text', 'clover'],
 
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/**/*.test.js',
-  ],
+  // collectCoverageFrom: [
+  //   'src/**/*.js',
+  //   '!src/**/*.test.js',
+  // ],
 
-  // Устанавливаем корневой каталог
-  roots: [process.cwd()],
-  rootDir: process.cwd(),
-  modulePaths: [process.cwd()],
+  // // Устанавливаем корневой каталог
+  // roots: [process.cwd()],
+  // rootDir: process.cwd(),
+  // modulePaths: [process.cwd()],
 
-  // Добавляем для правильной работы с путями
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/coverage/',
-    '/__tests__/',
-  ],
+  // // Добавляем для правильной работы с путями
+  // coveragePathIgnorePatterns: [
+  //   '/node_modules/',
+  //   '/coverage/',
+  //   '/__tests__/',
+  // ],
 
   transform: {},
 
-  coverageProvider: 'v8',
+  // coverageProvider: 'v8',
 }
